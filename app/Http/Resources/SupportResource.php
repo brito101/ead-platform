2972bd77ad4f7eca65fce5eab11e5423029e936a
brito101 <rodrigobrito101@hotmail.com>
2022-09-28 16:17:08 -0300

    Fix reply resource

--- app/Http/Resources/SupportResource.php
+++ app/Http/Resources/SupportResource.php
@@ -22,8 +22,8 @@ class SupportResource extends JsonResource
             'description' => $this->description,
             'user' => new UserResource($this->user),
             'lesson' => new LessonResource($this->whenLoaded('lessons')),
-            'replies' => LessonResource::collection($this->whenLoaded('replies')),
-            'dt_updated' => Carbon::make($this->updated_at)->format('Y-m-d H:i:s'),
+            'replies' => ReplySupportResource::collection($this->whenLoaded('replies')),
+            'dt_updated' => Carbon::make($this->updated_at)->format('d/m/Y H:i:s'),
         ];
     }
 }
